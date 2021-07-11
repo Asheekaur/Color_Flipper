3a658e93d0d53276fa9b5d1a8b7e7aabfd4ec5c5
Asheekaur <asheedhiman55@gmail.com>
2021-07-11 21:36:56 +0530

    generate random hex number

--- hex.js
+++ hex.js
@@ -1,2 +1,13 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const hex_length = 6;
+
+const btn = document.querySelector(".btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+    let randomColor = "#";
+    // generate a random hex number of hex_length
+    for(let i = 0 ;i < hex_length ; i++)
+    randomColor += hex[Math.floor(Math.random() * hex.length)];
+    
+});
