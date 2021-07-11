25cdf638ef82c15b619ea80ee2dd9d75b6d8d465
Asheekaur <asheedhiman55@gmail.com>
2021-07-11 21:31:08 +0530

    button click event added

--- simple.js
+++ simple.js
@@ -1,1 +1,11 @@
 const colors = ["cadetblue", "plum", "khaki", "lightcoral", "peru"];
+const btn = document.querySelector(".btn");
+const color = document.querySelector(".color");
+
+
+btn.addEventListener("click", function () {
+    const randomNum = getRandomNumber();
+
+    document.body.style.backgroundColor = colors[randomNum];
+    color.textContent = colors[randomNum];
+});
